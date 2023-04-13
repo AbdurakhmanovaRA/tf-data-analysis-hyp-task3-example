5b1b76bedc47d864433a467c8ec0db38363ac854
Regina <48201923+AbdurakhmanovaRA@users.noreply.github.com>
2023-04-13 23:42:44 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,8 +5,6 @@ import scipy.stats as stats
 chat_id = 241769496 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    p_value = stats.permutation_test(x, y,
-                           method='exact',
-                           func=lambda x, y: np.corrcoef(x, y)[1][0],
-                           seed=0)
+    p_value = stats.permutation_test(x, y, statistic, vectorized=True,
+                       n_resamples=np.inf, alternative='less')
     return p_value >= 0.7 # Ваш ответ, True или False
