cda87952d22480a200b3f32c3d2a6d17b035aac9
Regina <48201923+AbdurakhmanovaRA@users.noreply.github.com>
2023-04-13 23:24:20 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import numpy as np
+import scipy.stats as stats
 
+chat_id = 241769496 # Ваш chat ID, не меняйте название переменной
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
-
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+def solution(x: np.array, y: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+    p_value = stats.permutation_test(x, y,
+                           method='exact',
+                           func=lambda x, y: np.corrcoef(x, y)[1][0],
+                           seed=0)
+    return p_value >= 0.7 # Ваш ответ, True или False
